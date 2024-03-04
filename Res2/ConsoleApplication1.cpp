0a3ceaf4a66f006561ad88d4ae4097e3ac0d0a2a
gerzson7 <kaneva21012005@gmail.com>
2024-03-04 22:54:43 +0300

    change CMakeLists

---
+++ Res2/ConsoleApplication1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+int main() {
+	unsigned short a = 58;
+	//3 = 0b0111010
+	unsigned short i;
+	std::cin >> i;
+	unsigned short s = pow(2, i);
+	bool res = s & a;
+	std::cout << res;
+}
